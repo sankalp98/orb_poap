1b38c1ab27b645ed9bd35d98f590c67a87a89ff5
sankalp98 <sankalp.kasle@gmail.com>
2023-04-05 13:58:24 -0700

    modified:   src/pages/_app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from 'next/app'
 import { configureChains, createClient, WagmiConfig } from 'wagmi';
 import { mainnet, polygon } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public'
-import { LensProvider, LensConfig, staging, production } from '@lens-protocol/react-web';
+import { LensProvider, LensConfig, production } from '@lens-protocol/react-web';
 import { bindings as wagmiBindings } from '@lens-protocol/wagmi';
 
 const { provider, webSocketProvider } = configureChains([polygon, mainnet], [publicProvider()]);
